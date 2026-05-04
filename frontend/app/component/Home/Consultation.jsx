8b95b264323103d953091770c472e17d390d6c8c
varunmathur19 <varunmathur817@gmail.com>
2026-05-04 17:31:53 +0530

    fix the issue in responsie

--- frontend/app/component/Home/Consultation.jsx
+++ frontend/app/component/Home/Consultation.jsx
@@ -15,13 +15,13 @@ const Consultation = () => {
           <div className="absolute inset-0" />
 
           <div className="absolute inset-0 flex flex-col items-center justify-center px-4 text-center">
-            <h2 className="font-['Optima','Optima_LT_Pro',Candara,'Segoe_UI',sans-serif] md:text-[30px] font-[500] uppercase leading-[38px] tracking-normal text-white xl:text-[36px] sm:leading-[44px] lg:text-[30px] lg:leading-[49px]">
+            <h2 className="font-['Optima','Optima_LT_Pro',Candara,'Segoe_UI',sans-serif] text-[15px] md:text-[30px] font-[500] uppercase leading-[38px] tracking-normal text-white xl:text-[36px] sm:leading-[44px] lg:text-[30px] lg:leading-[49px]">
               GET YOUR PERSONALIZED OFFER
             </h2>
 
             <button
               type="button"
-              className="mt-0 md:mt-3 lg:mt-5 xl:mt-8  cursor-pointer inline-flex items-center gap-1 md:gap-3 rounded-full bg-white px-3 md:px-6 py-3 font-['Arial',sans-serif] text-[14px] md:text-[16px] font-normal leading-[100%] tracking-normal text-[#652A27]"
+              className="mt-0 md:mt-3 lg:mt-5 xl:mt-8  cursor-pointer inline-flex items-center gap-1 md:gap-3 rounded-full bg-white px-3 md:px-6 py-3 font-['Arial',sans-serif] text-[14px] md:text-[16px]  font-normal leading-[100%] tracking-normal text-[#652A27]"
             >
               Get consultation
               <i className="ri-arrow-right-line text-[18px]" />
