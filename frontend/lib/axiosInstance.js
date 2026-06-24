f8a25ee962777b450c39fc521463d8e1f11e2339
varunmathur19 <varunmathur817@gmail.com>
2026-06-24 10:40:57 +0530

    Initial deploy  commit

--- frontend/lib/axiosInstance.js
+++ frontend/lib/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL
     ? `${process.env.NEXT_PUBLIC_API_URL}/api`
-    : "http://localhost:3001/api",
+    : "https://mansha-backend-dxti.onrender.com/",
   headers: {
     "Content-Type": "application/json",
   },
