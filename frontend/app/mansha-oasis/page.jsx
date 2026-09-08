97afaa2a0e3da68f84a5a1c1ef57b2a727456218
itzz-aryan-121 <aryan.22b0131042@abes.ac.in>
2026-09-08 16:01:04 +0530

    new changes

--- frontend/app/mansha-oasis/page.jsx
+++ frontend/app/mansha-oasis/page.jsx
@@ -9,7 +9,7 @@ import Section6 from '../component/ManshaOasis/Section6'
 import Section7 from '../component/ManshaOasis/Section7'
 import Section8 from '../component/ManshaOasis/Section8'
 import Section9 from '../component/ManshaOasis/Section9'
-import WalkthroughVideos from '../component/common/WalkthroughVideos'
+import LayoutPlans from '../component/ManshaOasis/LayoutPlans'
 import ConsultationCommon from '../component/common/Consultation-common'
 import TestimoniaCommon from '../component/common/Testimonia-common'
 const ManshaOasis = () => {
@@ -25,7 +25,7 @@ const ManshaOasis = () => {
       <div className="py-[35px] lg:py-[70px]">
       <Section7 />
       </div>
-      <WalkthroughVideos projectKey="mansha-oasis" />
+      <LayoutPlans />
       <Section8 />
       {/* <Section9 /> */}
       <TestimoniaCommon/>
