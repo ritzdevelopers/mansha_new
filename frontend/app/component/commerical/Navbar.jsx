08c7f2c6a24097559282134806a7b2ca5e47db70
itzz-aryan-121 <aryan.22b0131042@abes.ac.in>
2026-09-21 17:07:55 +0530

    new changes

--- frontend/app/component/commerical/Navbar.jsx
+++ frontend/app/component/commerical/Navbar.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 import BookASite from "../common/Book-a-site";
 import NavSideMenu from "../common/NavSideMenu";
